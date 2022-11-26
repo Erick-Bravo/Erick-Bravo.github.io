8370b3fc2c002592d0ec74a3d104f03e9738310b
Erick Bravo <67840199+Erick-Bravo@users.noreply.github.com>
2022-11-25 17:42:57 -0800

    powered by component finalized with breakpoints

--- src/PoweredBy.tsx
+++ src/PoweredBy.tsx
@@ -1,18 +1,30 @@
 import styled from "@emotion/styled";
 import { Typography } from "@mui/material";
+import { breakpoints } from "./StyleTheme/theme";
 
 const TechList = styled(Typography)({
   display: "flex",
   justifyContent: "center",
-  height: "40px",
   alignItems: "center",
+  height: "40px",
   color: "white",
 });
 
+const Powered = styled(Typography)({
+  fontSize: "10px",
+  marginRight: "18px",
+  [`@media(min-width: ${breakpoints.tablet}px)`]: {
+    fontSize: "12px"
+  },
+})
+
 const IconContainer = styled(Typography)({
-  fontSize: "12px",
+  fontSize: "10px",
   fontWeight: 600,
-  marginBottom: "3px"
+  marginBottom: "7px",
+  [`@media(min-width: ${breakpoints.tablet}px)`]: {
+    fontSize: "12px",
+  },
 });
 
 const imgHeight = "20px";
@@ -20,10 +32,12 @@ const imgHeight = "20px";
 const PoweredBy = () => {
   return (
     <TechList>
-      <Typography sx={{marginRight: "12px"}}>Portfolio Powered By:</Typography>
+      <Powered>Portfolio programmed with:</Powered>
       <IconContainer>
+        Next.js <img src="./next-js.svg" alt="next.js" height={imgHeight} /> -
         React <img src="./react-icon.svg" alt="react" height={imgHeight} /> -
         Material UI <img src="./mui-icon.svg" alt="mui" height={imgHeight} /> -
+        Emotion <img src="./emotion.png" alt="emotion" height={imgHeight} /> -
         GSAP <img src="./gsap-icon.svg" alt="gsap" height={imgHeight} />
       </IconContainer>
     </TechList>
