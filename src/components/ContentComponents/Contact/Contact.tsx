f9f287c69c39224056f8c7ae2c6c1d586939b656
Erick Bravo <67840199+Erick-Bravo@users.noreply.github.com>
2022-11-25 21:02:50 -0800

    contact info

--- src/components/ContentComponents/Contact/Contact.tsx
+++ src/components/ContentComponents/Contact/Contact.tsx
@@ -1,16 +1,33 @@
 import styled from "@emotion/styled";
-import { Box } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import Header from "../Header/Header";
 
 const ContactContainer = styled(Box)({
   marginBottom: "40px",
 });
 
+const InfoContainer = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  marginTop: "30px",
+});
+
 const Contact = () => {
   return (
     <ContactContainer>
       <Header text="Contact" />
-      Contact Component
+      <InfoContainer>
+        <Typography sx={{ fontWeight: 600 }}>Erick Bravo</Typography>
+        <Typography>
+          Email:{" "}
+          <Link href="mailto:erickbravo120@gmail.com?subject=Subject&body=Body%20goes%20here'">
+            erickbravo120@gmail.com
+          </Link>
+        </Typography>
+        <Typography>phone: (909) 354-6272</Typography>
+      </InfoContainer>
     </ContactContainer>
   );
 };
