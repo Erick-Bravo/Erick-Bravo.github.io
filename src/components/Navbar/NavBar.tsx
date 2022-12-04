d100866b6ff75ee726e4371bf1b10d5794654ede
Erick Bravo <67840199+Erick-Bravo@users.noreply.github.com>
2022-12-04 12:35:05 -0800

    navbar consolidation

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,41 +1,4 @@
-import styled from "@emotion/styled";
-import { Box, Button, Link } from "@mui/material";
-import { breakpoints } from "../../StyleTheme/theme";
-
-const NavBarContainer = styled(Box)({
-    visibility: "hidden",
-    height: 0,
-  [`@media(min-width: ${breakpoints.tablet}px)`]: {
-    visibility: "visible",
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    height: "80px",
-    backgroundColor: "#4B98AB",
-    width: "100%",
-    borderRadius: "4px",
-    padding: "0 40px",
-  },
-  [`@media(min-width: ${breakpoints.desktop}px)`]: {
-    padding: "0 150px"
-  },
-});
-
-const NavLink = styled(Link)({
-  textDecoration: "none",
-  textDecorationLine: "none",
-});
-
-const ButtonStyled = styled(Button)({
-  width: "150px",
-  color: "white",
-  fontWeight: 600,
-  background: "#68B3C6",
-  textTransform: "none",
-  ":hover": {
-    background: "#84CFE2",
-  },
-});
+import { ButtonStyled, NavBarContainer, NavLink } from "./NavBar.styles";
 
 const NavBar = () => {
   return (
