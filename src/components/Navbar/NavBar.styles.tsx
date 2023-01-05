302d1b2faadfd1b531ca5e4971e93c3d61dd9c31
Erick Bravo <67840199+Erick-Bravo@users.noreply.github.com>
2023-01-05 12:46:38 -0800

    navbar now on mobile

--- src/components/Navbar/NavBar.styles.tsx
+++ src/components/Navbar/NavBar.styles.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 import { breakpoints } from "../../StyleTheme/theme";
 
 export const NavBarContainer = styled(Box)({
-  visibility: "hidden",
   position: "sticky",
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center",
   zIndex: 10,
   borderRadius: "0 0 4px 4px",
   top: 0,
-  height: 0,
+  backgroundColor: "#3e8b9e",
+  height: "85px",
+
   [`@media(min-width: ${breakpoints.tablet}px)`]: {
-    visibility: "visible",
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
+    // visibility: "visible",
     height: "80px",
-    backgroundColor: "#3e8b9e",
     width: "100%",
     padding: "0 40px",
   },
@@ -31,13 +31,17 @@ export const NavLink = styled(Link)({
 });
 
 export const ButtonStyled = styled(Button)({
-  width: "150px",
   color: "white",
   fontWeight: 600,
   background: "#68B3C6",
   textTransform: "none",
+  width: "90px",
+  height: "45px",
   ":hover": {
     background: "#84CFE2",
+  },
+  [`@media(min-width: ${breakpoints.tablet}px)`]: {
+    width: "150px",
   },
 });
 
@@ -50,14 +54,23 @@ const anchorStyles: object = {
 export const SummaryAnchor = styled(Box)({
   ...anchorStyles,
   top: "-120px",
+  [`@media(min-width: ${breakpoints.tablet}px)`]: {
+    top: "-120px",
+  },
 });
 
 export const ProjectsAnchor = styled(Box)({
   ...anchorStyles,
   top: "-120px",
+  [`@media(min-width: ${breakpoints.tablet}px)`]: {
+    top: "-120px",
+  },
 });
 
 export const ContactAnchor = styled(Box)({
   ...anchorStyles,
-  top: "-50px",
+  top: "0px",
+  [`@media(min-width: ${breakpoints.tablet}px)`]: {
+    top: "-50px",
+  },
 });
